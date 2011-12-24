af8f92d927aa89b0a0f9140b39f0e1ce87d2b46e
Villu Ruusmann <villu.ruusmann@gmail.com>
2011-12-24 11:51:35 +0000

    Updated JAXB2 Basics dependency from 0.5.2 to 0.6.3
    git-svn-id: http://jpmml.googlecode.com/svn/trunk@50 8c2e3fdc-7c96-1d1d-9acc-360cfccb4fad

--- pmml-core/src/main/java/org/dmg/pmml/PMMLObject.java
+++ pmml-core/src/main/java/org/dmg/pmml/PMMLObject.java
@@ -7,17 +7,18 @@ import java.io.*;
 
 import javax.xml.bind.annotation.*;
 
-import org.apache.commons.lang.builder.*;
-
 import org.jvnet.jaxb2_commons.lang.*;
+import org.jvnet.jaxb2_commons.locator.*;
 
 @XmlTransient
 abstract
 public class PMMLObject implements Equals, HashCode, Serializable {
 
-	public void equals(Object object, EqualsBuilder equalsBuilder){
+	public boolean equals(ObjectLocator thisLocator, ObjectLocator thatLocator, Object that, EqualsStrategy equalsStrategy){
+		return true;
 	}
 
-	public void hashCode(HashCodeBuilder hashCodeBuilder){
+	public int hashCode(ObjectLocator thisLocator, HashCodeStrategy hashCodeStrategy){
+		return 1;
 	}
 }
