dc5df66d08a236d6a0a75ff59e8bab16392e0cfd
Villu Ruusmann <villu.ruusmann@gmail.com>
2012-03-01 16:02:57 +0000

    Annotating class model with PMML schema versions
    git-svn-id: http://jpmml.googlecode.com/svn/trunk@55 8c2e3fdc-7c96-1d1d-9acc-360cfccb4fad

---
+++ pmml-schema/src/main/java/org/dmg/pmml/Version.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2012 University of Tartu
+ */
+package org.dmg.pmml;
+
+public enum Version {
+	PMML_3_0("http://www.dmg.org/PMML-3_0"),
+	PMML_3_1("http://www.dmg.org/PMML-3_1"),
+	PMML_3_2("http://www.dmg.org/PMML-3_2"),
+	;
+
+	private String uri = null;
+
+
+	private Version(String uri){
+		setURI(uri);
+	}
+
+	public String getURI(){
+		return this.uri;
+	}
+
+	private void setURI(String uri){
+		this.uri = uri;
+	}
+}
