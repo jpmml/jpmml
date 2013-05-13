af5047b1e7b04f5732d5da98f0d8827784adcd2d
Villu Ruusmann <villu.ruusmann@gmail.com>
2013-05-13 21:24:26 +0100

    Added Evaluator API usage example

--- pmml-evaluator/src/main/java/org/jpmml/evaluator/Evaluator.java
+++ pmml-evaluator/src/main/java/org/jpmml/evaluator/Evaluator.java
@@ -9,7 +9,43 @@ import org.jpmml.manager.*;
 
 import org.dmg.pmml.*;
 
+/**
+ * <pre>
+ * PMML pmml = ...;
+ * PMMLManager pmmlManager = new PMMLManager(pmml);
+ * Evaluator evaluator = (Evaluator)pmmlManager.getModelManager(null, ModelEvaluatorFactory.getInstance());
+ *
+ * Map<FieldName, Object> parameters = new LinkedHashMap<FieldName, Object>();
+ * List<FieldName> activeFields = evaluator.getActiveFields();
+ * for(FieldName activeField : activeFields){
+ *   parameters.put(activeField, ...);
+ * }
+ *
+ * Map<FieldName, ?> result = evaluator.evaluate(parameters);
+ *
+ * FieldName target = evaluator.getTarget();
+ * Object value = result.get(target);
+ *
+ * // Decode complex value to simple value
+ * if(value instanceof Computable){
+ *   Computable<?> computable = (Computable<?>)value;
+ *
+ *   value = computable.getResult();
+ * }
+ * </pre>
+ */
 public interface Evaluator extends Consumer {
+
+	/**
+	 * Convenience method for retrieving the predicted field.
+	 *
+	 * @return The predicted field
+	 *
+	 * @throws ModelManagerException If the number of predicted fields is not exactly one
+	 *
+	 * @see Consumer#getPredictedFields()
+	 */
+	FieldName getTarget();
 
 	/**
 	 * @param parameters Map of {@link #getActiveFields() active field values}.
@@ -18,7 +54,7 @@ public interface Evaluator extends Consumer {
 	 * Simple values should be represented using the Java equivalents of PMML data types (eg. String, Integer, Float, Double etc.).
 	 * Complex values should be represented as instances of {@link Computable} that return simple values.
 	 *
-	 * @throws EvaluationException If the evaluation failed.
+	 * @throws EvaluationException If the evaluation fails
 	 *
 	 * @see Computable
 	 */
