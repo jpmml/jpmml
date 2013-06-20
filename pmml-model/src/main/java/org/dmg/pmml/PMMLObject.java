b00778fa6eb320e5a71f4a968125389475901471
Villu Ruusmann <villu.ruusmann@gmail.com>
2013-06-20 12:44:00 +0100

    Enabled SAX Locator information

--- pmml-model/src/main/java/org/dmg/pmml/PMMLObject.java
+++ pmml-model/src/main/java/org/dmg/pmml/PMMLObject.java
@@ -7,12 +7,14 @@ import java.io.*;
 
 import javax.xml.bind.annotation.*;
 
+import com.sun.xml.bind.*;
+
 import org.jvnet.jaxb2_commons.lang.*;
 import org.jvnet.jaxb2_commons.locator.*;
 
 @XmlTransient
 abstract
-public class PMMLObject implements Equals, HashCode, ToString, Serializable {
+public class PMMLObject implements Equals, HashCode, Locatable, ToString, Serializable {
 
 	public boolean equals(ObjectLocator thisLocator, ObjectLocator thatLocator, Object that, EqualsStrategy equalsStrategy){
 		return true;
