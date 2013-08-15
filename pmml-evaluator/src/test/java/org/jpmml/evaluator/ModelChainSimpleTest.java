b0d12b0b09106893fc308618aa9cdd68befb652f
Villu Ruusmann <villu.ruusmann@gmail.com>
2013-08-15 23:04:59 +0100

    Added unit test for 'modelChain' multiple method type

---
+++ pmml-evaluator/src/test/java/org/jpmml/evaluator/ModelChainSimpleTest.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2013 University of Tartu
+ */
+package org.jpmml.evaluator;
+
+import java.util.*;
+
+import org.dmg.pmml.*;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+public class ModelChainSimpleTest extends ModelChainTest {
+
+	@Test
+	public void evaluate() throws Exception {
+		Map<FieldName, ?> result = evaluateExample(1.4, 0.2);
+
+		assertNotNull(result.get(new FieldName("PollenIndex")));
+	}
+}
