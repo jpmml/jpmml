f47129eed0ee8493b8a0b13b48f19fafccd84591
Villu Ruusmann <villu.ruusmann@gmail.com>
2013-08-29 15:43:32 +0100

    Removed obsolete methods

--- pmml-evaluator/src/main/java/org/jpmml/evaluator/EvaluatorUtil.java
+++ pmml-evaluator/src/main/java/org/jpmml/evaluator/EvaluatorUtil.java
@@ -31,59 +31,21 @@ public class EvaluatorUtil {
 
 	/**
 	 * Decouples a {@link Map} instance from the current runtime environment by decoding both its keys and values.
-	 *
-	 * @see #decodeKeys(Map)
-	 * @see #decodeValues(Map)
 	 */
 	static
 	public Map<String, ?> decode(Map<FieldName, ?> map){
-		return decodeKeys(decodeValues(map));
-	}
+		Map<String, Object> result = Maps.newLinkedHashMap();
 
-	/**
-	 * Replaces String keys with {@link FieldName} keys.
-	 */
-	static
-	public <V> Map<FieldName, V> encodeKeys(Map<String, V> map){
-		Map<FieldName, V> result = Maps.newLinkedHashMap();
+		Collection<? extends Map.Entry<FieldName, ?>> entries = map.entrySet();
+		for(Map.Entry<FieldName, ?> entry : entries){
+			FieldName key = entry.getKey();
+			Object value = entry.getValue();
 
-		Collection<Map.Entry<String, V>> entries = map.entrySet();
-		for(Map.Entry<String, V> entry : entries){
-			result.put(FieldName.create(entry.getKey()), entry.getValue());
-		}
+			if(key == null){
+				continue;
+			}
 
-		return result;
-	}
-
-	/**
-	 * Replaces {@link FieldName} keys with String keys.
-	 *
-	 * @see FieldName#getValue()
-	 */
-	static
-	public <V> Map<String, V> decodeKeys(Map<FieldName, V> map){
-		Map<String, V> result = Maps.newLinkedHashMap();
-
-		Collection<Map.Entry<FieldName, V>> entries = map.entrySet();
-		for(Map.Entry<FieldName, V> entry : entries){
-			result.put((entry.getKey()).getValue(), entry.getValue());
-		}
-
-		return result;
-	}
-
-	/**
-	 * Replaces {@link Computable} complex values with simple values.
-	 *
-	 * @see Computable
-	 */
-	static
-	public <K> Map<K, ?> decodeValues(Map<K, ?> map){
-		Map<K, Object> result = Maps.newLinkedHashMap();
-
-		Collection<? extends Map.Entry<K, ?>> entries = map.entrySet();
-		for(Map.Entry<K, ?> entry : entries){
-			result.put(entry.getKey(), decode(entry.getValue()));
+			result.put(key.getValue(), decode(value));
 		}
 
 		return result;
